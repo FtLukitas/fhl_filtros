c98bb21de0a621b6ff6bab65ee197b95fd6480d7
FtLukitas <lukitabrachito@gmail.com>
2026-05-17 12:42:56 -0300

    fix de ip allowed

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Agregá la IP de tu celular acá
-  allowedDevOrigins: ['192.168.1.43', 'localhost'],
-  allowedDevOrigins: ['192.168.100.36', 'localhost'],
+  allowedDevOrigins: ['192.168.1.43', '192.168.100.36', 'localhost'],
 };
-
 export default nextConfig;
